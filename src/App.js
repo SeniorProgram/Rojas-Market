748b9a318ffa406ef6752285d541fec392dc7d60
Tu Nombre <victorquichica@gmail.com>
2024-11-20 18:05:47 -0500

    version1.0

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <span>Informe de Datos</span>
+        <div className="superior">
+        <h1>Proyecto</h1>
+        </div>
+        <div className="inferior">
+          <h2>Contenido</h2>
+        </div>
+
+        <footer>
+          <h4>Desarrollado : Ing. Melendez</h4>
+          <h5>Celular:959818192</h5>
+        </footer>
+
+
                 
         
       </header>
